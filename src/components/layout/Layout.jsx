648201be7e89951fe5649af57cd8bfa9c1c5e8cf
Tc3-Tech <t.clarke.iii@gmail.com>
2025-05-18 18:20:10 -0700

    Updates

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,21 @@
+// src/components/Layout/Layout.jsx
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Sidebar from './Sidebar';
+
+const Layout = () => {
+  return (
+    <div className="flex h-screen bg-gray-100 font-sans">
+      <Sidebar />
+      <div className="flex flex-col flex-1 overflow-hidden">
+        <Header />
+        <main className="flex-1 overflow-y-auto p-6 bg-gray-100">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
